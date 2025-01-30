3e8f130ac151187157a3b0c84f5bb9b7b840970e
quadrinex <vicraft2155@gmail.Com>
2025-01-30 17:50:09 +0100

    made the entire code lol

--- index.js
+++ index.js
@@ -1,2 +1,40 @@
 import dotenv from 'dotenv'
 dotenv.config()
+
+import {
+    Client,
+    GatewayIntentBits,
+    ButtonBuilder,
+    ButtonStyle,
+    ModalBuilder,
+    TextInputBuilder,
+    TextInputStyle
+} from 'discord.js';
+
+const client = new Client({
+    intents: [
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.GuildMembers,
+        GatewayIntentBits.DirectMessages
+    ],
+});
+
+client.login(process.env.DISCORD_TOKEN);
+
+const btn = new ButtonBuilder()
+        .setCustomId('test')
+        .setLabel('Test the bot')
+        .setStyle(ButtonStyle.Primary);
+
+client.on("messageCreate", async (message) => {
+
+    console.log(message)
+
+    if (!message?.author.bot) {
+        message.author.send({
+            content: 'Try the bot!',
+            components: [btn]
+        });
+    }
+});
